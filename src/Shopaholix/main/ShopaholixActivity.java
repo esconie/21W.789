2ef24d6b3d94ce7f4bc0bc0b872b0e5170fc34e8
David Field <dfield@mit.edu>
2012-03-20 00:51:34 -0400

    Didn't mean to make last commit, reverting.

--- src/Shopaholix/main/ShopaholixActivity.java
+++ src/Shopaholix/main/ShopaholixActivity.java
@@ -1,15 +1,12 @@
 package Shopaholix.main;
 
-import java.io.IOException;
-
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 import android.widget.TextView;
 
 public class ShopaholixActivity extends Activity {
-   
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,15 +14,8 @@ public class ShopaholixActivity extends Activity {
         setContentView(R.layout.main);
         
     }
-    public void myButtonPressed(View v) throws IOException{
-    	TextView t = (TextView)this.findViewById(R.id.textView1);
-    	t.setText("Running");
-    	new DownloadFile((Context)this).execute();
-    	
-    }
-    public void onProgessUpdate(String... args) throws IOException{
+    public void myButtonPressed(View v){
         TextView t = (TextView)this.findViewById(R.id.textView1);
-        t.setText("Done");
-        
+        t.setText("Testing commitqewrger re");
     }
 }
