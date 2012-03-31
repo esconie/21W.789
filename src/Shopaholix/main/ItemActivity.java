5bf58ccba8bc3fdcc2bf29187bd1a59160109a0e
lihaoyi <haoyi.sg@gmail.com>
2012-03-30 23:12:54 -0400

    did lots of stuff

---
+++ src/Shopaholix/main/ItemActivity.java
@@ -0,0 +1,31 @@
+package Shopaholix.main;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.TextView;
+import android.location.*;
+public class ItemActivity extends Activity {
+    /** Called when the activity is first created. */
+	ImageView greenUp;
+	ImageView yellowMid;
+	ImageView redDown;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_view);
+    }
+    public void clickUp(View v){
+    	
+    }
+    public void clickMid(View v){
+    	
+    }
+	public void clickDown(View v){
+		
+	}
+    
+}
