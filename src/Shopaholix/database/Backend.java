37f3d6b19928a7755a53ef2627dbcb921bef256f
dfield <dfield@18.189.100.131>
2012-04-02 18:09:54 -0400

    Made singleton backend

--- src/Shopaholix/database/Backend.java
+++ src/Shopaholix/database/Backend.java
@@ -8,8 +8,10 @@ import java.util.PriorityQueue;
 
 import Shopaholix.database.ItemRatings.Rating;
 import android.content.Context;
+import android.util.Log;
 
 public class Backend {
+	static Backend backend=new Backend();
 	HashMap<String, Item> items = new HashMap<String, Item>();
 	private User me;
 	private HashSet<User> users;
@@ -20,6 +22,25 @@ public class Backend {
 		users = new HashSet<User>();
 		allTags = new HashSet<Tag>();
 		users.add(me);
+		String[] upcs={"037000188421","037000230113","037000188438","037000185055","037000185208"};
+		for(String upc:upcs){
+			backend.getItem(upc);
+		}
+		User haoyi=new User("Haoyi");
+		backend.addFamilyMember(haoyi);
+		backend.rateItem(upcs[0], Rating.GOOD);
+		backend.rateItem(upcs[1], Rating.GOOD);
+		backend.rateItem(upcs[3], Rating.NEUTRAL);
+		backend.rateItem(upcs[4], Rating.BAD);
+		backend.rateFamilyItem(upcs[0], haoyi, Rating.GOOD);
+		backend.rateFamilyItem(upcs[1],haoyi,Rating.BAD);
+		Log.d("BackendTest",backend.getItem(upcs[0]).toString());
+		Log.d("BackendTest",backend.getFamilyMembers().toString());
+		ArrayList<Tag> requiredTags=new ArrayList<Tag>();
+		Log.d("BackendTest",backend.getSuggestedItems(requiredTags,new Tag(""),1).toString());
+		Log.d("BackendTest",backend.getSuggestedTags(requiredTags,new Tag("")).toString());
+		Log.d("BackendTest",backend.getSuggestedItems(requiredTags,new Tag("pot")).toString());
+		Log.d("BackendTest",backend.getSuggestedTags(requiredTags,new Tag("sta")).toString());
 	}
 
 	public ArrayList<Item> getSuggestedItems(ArrayList<Tag> tags,Tag partial) {
