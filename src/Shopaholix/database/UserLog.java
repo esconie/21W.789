c301eedba209f4c4cc0ab6a3a6520d1ed4f6863a
Danny Chiao <dchiao@mit.edu>
2012-04-22 00:05:50 -0400

    Class for logging information

---
+++ src/Shopaholix/database/UserLog.java
@@ -0,0 +1,60 @@
+package Shopaholix.database;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import android.os.Environment;
+
+public class UserLog {
+
+	public static void sendToServer() throws UnknownHostException, IOException{
+		Socket sock = new Socket("23.21.127.158", 789);
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));        
+        String log = readLogAsString();
+        out.println(log);
+        out.flush();
+	}
+	
+	private static String readLogAsString() throws java.io.IOException{
+		File logFile = new File(Environment.getExternalStorageDirectory(), Backend.backend.ID + ".log");
+	    byte[] buffer = new byte[(int) logFile.length()];
+	    BufferedInputStream f = null;
+	    try {
+	        f = new BufferedInputStream(new FileInputStream(logFile));
+	        f.read(buffer);
+	    } finally {
+	        if (f != null) try { f.close(); } catch (IOException ignored) { }
+	    }
+	    return new String(buffer);
+	}
+	
+	public static void appendLog(String text) {
+		File logFile = new File(Environment.getExternalStorageDirectory(), Backend.backend.ID + ".log");
+		if (!logFile.exists()) {
+			try {
+				logFile.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		try {
+			// BufferedWriter for performance, true to set append to file flag
+			BufferedWriter buf = new BufferedWriter(new FileWriter(logFile,
+					true));
+			buf.append(text);
+			buf.newLine();
+			buf.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
